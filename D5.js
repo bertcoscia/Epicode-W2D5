64059a16324db0dc53f38e3792002967934327ff
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-05-10 11:22:42 +0200

    D5.js -> answered q8

--- D5.js
+++ D5.js
@@ -104,6 +104,14 @@ console.log(justTrims);
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 
+console.log(cars);
+for (let i = 0; i < cars.length; i++)
+  if (cars[i].color.charAt(0) === "b") {
+    console.log("Fizz");
+  } else {
+    console.log("Buzz");
+  }
+
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */

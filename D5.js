0c138b9ac8fd52d13210d61c77d2c314e39b8ec4
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-05-10 11:42:45 +0200

    D5.js -> answered q9

--- D5.js
+++ D5.js
@@ -117,6 +117,12 @@ for (let i = 0; i < cars.length; i++)
 */
 const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
 
+let counter = 0;
+while (numericArray[counter] !== 32) {
+  console.log(numericArray[counter]);
+  counter++;
+}
+
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
